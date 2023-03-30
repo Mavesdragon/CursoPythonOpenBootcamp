e55f2efb20c39149fee3865940b35228dbda1f22
Mavesdragon <dmanas1997@uoc.edu>
2023-03-30 10:08:03 +0200

    30032023

---
+++ demoCurso/Ejercicio 2 tema6.py
@@ -0,0 +1,16 @@
+class Alumno:
+
+    def isAbrobado(self, nota):
+        if nota >= 5:
+            print('Estas aprobado con una nota de ' + str(nota))
+        else:
+            print('Estas suspendido con una nota de ' + str(nota))
+
+    def __init__(self,nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+
+a = Alumno('Pedro', 5)
+
+print(a.isAbrobado(4))
+

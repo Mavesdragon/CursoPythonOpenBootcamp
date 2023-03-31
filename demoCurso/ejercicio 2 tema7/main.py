2ad227dff7a8e823beeebcf45deeca8970b1b669
Mavesdragon <dmanas1997@uoc.edu>
2023-03-31 09:50:16 +0200

    30032023

--- demoCurso/ejercicio 2 tema7/main.py
+++ demoCurso/ejercicio 2 tema7/main.py
@@ -1,10 +1,19 @@
-import time as tm
+import datetime as tm
 import pprint
 
 def main():
 
     pprint.pprint(dir(tm))
-    tm.get_clock_info()
+    hora_actual = tm.datetime.now()
+
+
+    if hora_actual.hour>=19 and hora_actual.minute>=0 and hora_actual.second>=0:
+        print("Vete a casa empanao!")
+
+    else:
+        horas_que_quedan = 19 - int(hora_actual.hour)
+        print('Te quedan ', horas_que_quedan, 'para irte a casa')
+
 
 
 
